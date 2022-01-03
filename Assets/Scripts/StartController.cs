bd83a0ca6e60a41d809a9eb7fe651f3496b6d263
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2022-01-03 22:49:08 +0600

    Добавил анимацию движения камеры

--- Assets/Scripts/StartController.cs
+++ Assets/Scripts/StartController.cs
@@ -1,12 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace CustomConsole
 {
     public class StartController : MonoBehaviour
     {
         [SerializeField] private Camera _camera;
+        [SerializeField] private Button _start;
+        [SerializeField] private GameObject _followObj;
 
         private GameProfile _gameProfile;
         private MainController _mainController;
@@ -15,6 +19,13 @@ namespace CustomConsole
         {
             _gameProfile = new GameProfile();
             _mainController = new MainController(_gameProfile, _camera);
+            _start.onClick.AddListener(StartGame);
+        }
+
+        private void StartGame()
+        {
+            _start.gameObject.SetActive(false);
+            _followObj.SetActive(true);
         }
 
         private void Update()
