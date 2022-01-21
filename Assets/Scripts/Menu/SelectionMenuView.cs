0e20bc7868a47b55e39730a58b94f5478a6c7c80
mrak1444 <74318127+mrak1444@users.noreply.github.com>
2022-01-22 01:07:27 +0600

    Переделал меню выбора

--- Assets/Scripts/Menu/SelectionMenuView.cs
+++ Assets/Scripts/Menu/SelectionMenuView.cs
@@ -9,13 +9,10 @@ namespace CustomConsole
     {
         [SerializeField] private Button _buttonStart;
         [SerializeField] private Button _settingsButton;
-        [SerializeField] private Button _buyButton;
         [SerializeField] private Button _settingsCloseButton;
-        [SerializeField] private Button _buyCloseButton;
         [SerializeField] private Button _sprayButton;
         [SerializeField] private GameObject _objMenu;
         [SerializeField] private GameObject _settingsMenu;
-        [SerializeField] private GameObject _buyMenu;
         [SerializeField] private GameObject _paintMenu;
 
         public void Init(UnityAction startSpray)
@@ -23,9 +20,7 @@ namespace CustomConsole
             //_buttonStart.onClick.AddListener(startGame);
             _buttonStart.onClick.AddListener(SelecMode);
             _settingsButton.onClick.AddListener(SettingsNenu);
-            _buyButton.onClick.AddListener(BuyMenu);
             _settingsCloseButton.onClick.AddListener(CloseButton);
-            _buyCloseButton.onClick.AddListener(CloseButton);
             _sprayButton.onClick.AddListener(startSpray);
         }
 
@@ -38,12 +33,6 @@ namespace CustomConsole
         private void CloseButton()
         {
             _settingsMenu.SetActive(false);
-            _buyMenu.SetActive(false);
-        }
-
-        private void BuyMenu()
-        {
-            _buyMenu.SetActive(true);
         }
 
         private void SettingsNenu()
